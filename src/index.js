5d9fb10d7fb8444ad0323cf174cbd30a4b0a4200
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-12-08 23:28:53 +0530

    added scrumble text effect

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import "./styles/index.css";
 import "./styles/scroll_down.scss";
 import "./styles/infinite_scrolling_text_section2.scss";
 import "./styles/block_text_reveal.scss"
+
+import './textScrumble'
